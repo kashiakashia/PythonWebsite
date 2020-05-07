5d97821b797726ef52321a0447c7e4d5ca97ad6e
Katarzyna Smelcerz <kasia.smelcerz@gmail.com>
2020-05-07 17:03:36 +0200

    init on flask template

--- script.py
+++ script.py
@@ -1,12 +1,18 @@
 # website made in python
-from flask import Flask
+from flask import Flask, render_template
 
+# special variable that gets as __name__ python script's name
 app = Flask(__name__)
 
 
-@app.route('/')  # home foler
+@app.route('/')  # homepage
 def home():
-    return "Hello World!"
+    return render_template("home.html")
+
+
+@app.route('/about/')  # about page
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
